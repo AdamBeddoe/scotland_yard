852269b85dc46453aee0b5342e147f21c4eae223
Adam <adam.beddoe@gmail.com>
2017-03-16 21:27:33 +0000

    A bunch of tidying up.

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -31,8 +31,8 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move> {
     private Graph<Integer, Transport> graph;
     private PlayerConfiguration mrX;
     private PlayerConfiguration firstDetective;
-    private List<PlayerConfiguration> detectives = new ArrayList<PlayerConfiguration>();
-    private List<ScotlandYardPlayer> playerList= new ArrayList<ScotlandYardPlayer>();
+    private List<PlayerConfiguration> startPlayers = new ArrayList<>();
+    private List<ScotlandYardPlayer> playerList= new ArrayList<>();
     private PlayerConfiguration currentPlayer;
     private Set<Move> availableMoves;
     private int roundNum = 0;
@@ -43,81 +43,58 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move> {
                 this.rounds = requireNonNull(rounds);
                 this.graph = requireNonNull(graph);
                 this.mrX = requireNonNull(mrX);
-                this.detectives.add(mrX);
+                this.startPlayers.add(mrX);
                 this.firstDetective = requireNonNull(firstDetective);
-                this.detectives.add(firstDetective);
+                this.startPlayers.add(firstDetective);
                 this.currentPlayer = mrX;
 
                 for(PlayerConfiguration detective : restOfTheDetectives) {
-                	this.detectives.add(requireNonNull(detective));
+                	this.startPlayers.add(requireNonNull(detective));
 				}
 
 				if (rounds.isEmpty()) throw new IllegalArgumentException("Empty rounds");
 	 			if (graph.isEmpty()) throw new IllegalArgumentException("Empty map (graph)");
 	 			if (mrX.colour != Black) throw new IllegalArgumentException("MrX should be Black");
 
-	 			checkDuplicateLocations(detectives);
-	 			checkDuplicateColours(detectives);
-	 			checkDetectiveTickets(detectives);
+	 			checkDuplicateLocations(startPlayers);
+	 			checkDuplicateColours(startPlayers);
+	 			checkPlayerTickets(startPlayers);
 
-	 			for(PlayerConfiguration detective : detectives){
-	 				playerList.add(new ScotlandYardPlayer(detective.player, detective.colour, detective.location, detective.tickets));
+	 			for(PlayerConfiguration player : startPlayers){
+	 				playerList.add(new ScotlandYardPlayer(player.player, player.colour, player.location, player.tickets));
 				}
 	}
 
-	// Checks all detectives for duplicate locations.
-	private void checkDuplicateLocations(List<PlayerConfiguration> detectives) {
+	// Checks all startPlayers for duplicate locations.
+	private void checkDuplicateLocations(List<PlayerConfiguration> startPlayers) {
 		Set<Integer> locations = new HashSet<>();
-		for(PlayerConfiguration detective : detectives) {
-			if (locations.contains(detective.location)) throw new IllegalArgumentException("Duplicate location");
-			else locations.add(detective.location);
+		for(PlayerConfiguration player : startPlayers) {
+			if (locations.contains(player.location)) throw new IllegalArgumentException("Duplicate location");
+			else locations.add(player.location);
 		}
 	}
 
-	// Checks all detectives for duplicate colours.
-    private void checkDuplicateColours(List<PlayerConfiguration> detectives) {
+	// Checks all startPlayers for duplicate colours.
+    private void checkDuplicateColours(List<PlayerConfiguration> startPlayers) {
         Set<Colour> colours = new HashSet<>();
-        for(PlayerConfiguration detective : detectives) {
-            if (colours.contains(detective.colour)) throw new IllegalArgumentException("Duplicate colour");
-            else colours.add(detective.colour);
+        for(PlayerConfiguration player : startPlayers) {
+            if (colours.contains(player.colour)) throw new IllegalArgumentException("Duplicate colour");
+            else colours.add(player.colour);
         }
     }
 
-	// Validates the tickets of all detectives.
-    private void checkDetectiveTickets(List<PlayerConfiguration> detectives) {
-		for (PlayerConfiguration detective : detectives) {
-			validateTicketTypes(detective);
-			if (!detective.equals(this.mrX)) validateOtherDetectiveTickets(detective);
+	// Validates the tickets of all startPlayers.
+    private void checkPlayerTickets(List<PlayerConfiguration> startPlayers) {
+		for (PlayerConfiguration player : startPlayers) {
+			if (player.tickets.size() != 5) throw new IllegalArgumentException("Player does not have required tickets");
+			if (!player.equals(this.mrX)) validateDetectiveTickets(player);
 		}
     }
- /*
-	// Checks if Mr X has the required number of each ticket.
-	private void validateMrXTickets(PlayerConfiguration detective) {
-		if (detective.tickets.get(Secret) != 5) throw new IllegalArgumentException("Mr X does not have secret ticket");
-		if (detective.tickets.get(Double) != 2) throw new IllegalArgumentException("Mr X does not have double ticket");
-		if (detective.tickets.get(Taxi) != 4) throw new IllegalArgumentException("Mr X does not have 4 Taxi tickets");
-		if (detective.tickets.get(Bus) != 3) throw new IllegalArgumentException("Mr X does not have 3 Bus tickets");
-		if (detective.tickets.get(Underground) != 3) throw new IllegalArgumentException("Mr X does not have 3 Underground tickets");
 
-	}
-*/
-	// Checks if a standard detective has the required number of each ticket.
-	private void validateOtherDetectiveTickets(PlayerConfiguration detective) {
-		if (detective.tickets.get(Secret) != 0) throw new IllegalArgumentException("Detective has a Secret ticket");
-		if (detective.tickets.get(Double) != 0) throw new IllegalArgumentException("Detective has a Double ticket");
-		//if (detective.tickets.get(Taxi) != 11) throw new IllegalArgumentException("Detective does not have 10 Taxi tickets");
-		//if (detective.tickets.get(Bus) != 8) throw new IllegalArgumentException("Detective does not have 8 Bus tickets");
-		//if (detective.tickets.get(Underground) != 4) throw new IllegalArgumentException("Detective does not have 4 Underground tickets");
-	}
-
-	// Checks if the ticket map of a detective contains all the required ticket types.
-	// Query whether checking length would be enough or whether this is better because of error information.
-    private void validateTicketTypes(PlayerConfiguration detective) {
-		if (!detective.tickets.containsKey(Secret)) throw new IllegalArgumentException("No ticket field for Secret");
-		if (!detective.tickets.containsKey(Double)) throw new IllegalArgumentException("No ticket field for Double");
-		if (!detective.tickets.containsKey(Taxi)) throw new IllegalArgumentException("No ticket field for Taxi");
-		if (!detective.tickets.containsKey(Bus)) throw new IllegalArgumentException("No ticket field for Bus");
-		if (!detective.tickets.containsKey(Underground)) throw new IllegalArgumentException("No ticket field for Underground");
+	// Checks a detective does not have any Mr X tickets.
+	private void validateDetectiveTickets(PlayerConfiguration player) {
+		if (player.tickets.get(Secret) != 0) throw new IllegalArgumentException("Detective has a Secret ticket");
+		if (player.tickets.get(Double) != 0) throw new IllegalArgumentException("Detective has a Double ticket");
 	}
 
 	@Override
@@ -134,7 +111,7 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move> {
 
 	@Override
 	public void startRotate() {
-		for (PlayerConfiguration p : detectives) {
+		for (PlayerConfiguration p : startPlayers) {
 			this.currentPlayer = p;
 			Player player = p.player;
 			if (p == mrX) {
@@ -171,8 +148,8 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move> {
 	public List<Colour> getPlayers() {
 		List<Colour> colours = new ArrayList<Colour>();
 
-		for(PlayerConfiguration detective : detectives){
-			colours.add(detective.colour);
+		for(PlayerConfiguration player : startPlayers){
+			colours.add(player.colour);
 		}
 
 		return Collections.unmodifiableList(colours);
@@ -189,13 +166,13 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move> {
 	public int getPlayerLocation(Colour colour) {
 		int location = 0;
 
-		for(PlayerConfiguration detective : detectives) {
-			if(detective.colour.equals(Black)){
+		for(PlayerConfiguration player : startPlayers) {
+			if(player.colour.equals(Black)){
 				if(!getRounds().get(getRounds().size()-1)) location = 0;
-				else return detective.location;
+				else return player.location;
 			}
-			else if(detective.colour.equals(colour)) {
-				location = detective.location;
+			else if(player.colour.equals(colour)) {
+				location = player.location;
 			}
 		}
 
@@ -204,9 +181,9 @@ public class ScotlandYardModel implements ScotlandYardGame, Consumer<Move> {
 
 	@Override
 	public int getPlayerTickets(Colour colour, Ticket ticket) {
-		for(PlayerConfiguration detective : detectives) {
-			if(detective.colour.equals(colour)) {
-				return detective.tickets.get(ticket);
+		for(PlayerConfiguration player : startPlayers) {
+			if(player.colour.equals(colour)) {
+				return player.tickets.get(ticket);
 			}
 		}
 
